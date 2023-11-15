3d9f3b394e3df42fdaef595cfc83423243cedcfe
filzasaleem <filza.saleem05@gmail.com>
2023-11-15 17:34:02 +0100

    worked a bit on footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import "./footer.css";
 export const Footer = () => {
   return (
-    <div className="footer">
-      <h4>A project by Filza Saleem, Kai Lee Hernandez and Diana Undén</h4>
-    </div>
+    <footer className="footer">
+      <p>A project by Filza Saleem, Kai Lee Hernandez and Diana Undén</p>
+    </footer>
   );
 };

98d43f3ec4889ef9144645215e46b865b3483adb
filzasaleem <filza.saleem05@gmail.com>
2023-11-15 15:01:08 +0100

    added feedbackpage styling

--- src/pages/QuestionFeedbackPage.jsx
+++ src/pages/QuestionFeedbackPage.jsx
@@ -1,22 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
-
 import { CurrentQuestion } from "../components/CurrentQuestion";
 import { goToNextQuestion } from "../reducers/quiz";
 import "./QuestionFeedbackPage.css";
 
 export const QuestionFeedbackPage = () => {
   const dispatch = useDispatch();
-
-  const currentQuestionIndex = useSelector(
-    (state) => state.quiz.currentQuestionIndex
-  );
-  const answers = useSelector(
-    (state) => state.quiz.answers[currentQuestionIndex]
-  );
-
-  const options = useSelector(
-    (state) => state.quiz.questions[currentQuestionIndex].options
-  );
+  const currentQuestionIndex = useSelector((state) => state.quiz.currentQuestionIndex);
+  const answers = useSelector((state) => state.quiz.answers[currentQuestionIndex]);
+  const options = useSelector((state) => state.quiz.questions[currentQuestionIndex].options);
+  const correctAnswerIndex = useSelector((state) => state.quiz.questions[currentQuestionIndex].correctAnswerIndex);
   const totalQuestions = useSelector((state) => state.quiz.questions.length);
 
   const handleGoToNextQuestion = () => {
@@ -35,12 +27,18 @@ export const QuestionFeedbackPage = () => {
       <div className="feedbackOptionContainer">
         <div>{correct ? "Correct!" : "Incorrect!"}</div>
         {options.map((option, index) => (
-          <div key={index} className="feedbackOptions" >
-            <button disabled> {option}</button>
+          <div key={index} className="feedbackOptions">
+            <button
+              className={`feedbackBtn ${index === correctAnswerIndex ? "correct" : index === answers.answerIndex ? "incorrect" : ""}`}
+              disabled
+            >
+              {option}
+            </button>
           </div>
         ))}
-
-        <button  className ="nextButton" onClick={handleGoToNextQuestion}>{currentQuestionIndex < totalQuestions-1 ? "Next Question" : "Check score"}</button>
+        <button className="nextButton" onClick={handleGoToNextQuestion}>
+          {currentQuestionIndex < totalQuestions - 1 ? "Next Question" : "Check score"}
+        </button>
       </div>
     </div>
   );

c3776702b0a3dff062e52387ea06b7712cbac92f
filzasaleem <filza.saleem05@gmail.com>
2023-11-15 17:12:22 +0100

    progress bar styling in progress

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,16 +1,28 @@
 // This is the component that will display the progress bar
 
 import { useSelector } from "react-redux";
-
+import "./ProgressBar.css";
 
 export const ProgressBar = () => {
   const totalQuestions = useSelector((state) => state.quiz.questions.length);
-  const currenQuestion = useSelector((state) => state.quiz.currentQuestionIndex)
-  console.log("total lenght",totalQuestions)
+  const currenQuestion = useSelector(
+    (state) => state.quiz.currentQuestionIndex
+  );
+  console.log("total lenght", totalQuestions);
   return (
-    <div>
-      <progress  value={currenQuestion+1} max={totalQuestions}>  </progress>
-      <span>{currenQuestion+1}/{totalQuestions}</span>
-    </div>
+    <>
+      <div className="progressBarContainer">
+        <progress
+          value={currenQuestion}
+          max={totalQuestions - 1}
+          className="progressBar"
+        >
+          {" "}
+        </progress>
+        <span>
+          {currenQuestion + 1}/{totalQuestions}
+        </span>
+      </div>
+    </>
   );
 };

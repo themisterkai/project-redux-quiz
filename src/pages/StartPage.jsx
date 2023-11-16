b45c924d0d69e385f4e6e48a0d0dff1441f49b29
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-11-16 21:37:23 +0100

    Adjust css to media screens (#21)
    * add styling to quiz
    
    * add scoreCounter to header
    
    * fix padding and gaps + classNames
    
    * adjust min-height for media screens

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,7 +1,7 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 
-import { quizTimerStart } from '../reducers/quiz';
-import './startPage.css';
+import { quizTimerStart } from "../reducers/quiz";
+import "./startPage.css";
 
 export const StartPage = () => {
   const dispatch = useDispatch();
@@ -9,13 +9,13 @@ export const StartPage = () => {
     dispatch(quizTimerStart()); //make it true
   };
 
-  const quizState = useSelector(state => state.quiz);
+  const quizState = useSelector((state) => state.quiz);
   const quizOver = quizState.quizOver;
   const quizTimerState = quizState.quizTimerState;
 
   const showStartPage = !quizOver && !quizTimerState;
-  
-  if (!showStartPage  ) {
+
+  if (!showStartPage) {
     return <></>;
   }
 
@@ -23,13 +23,19 @@ export const StartPage = () => {
     <div className="startPage">
       <h1>Welcome to our quiz!</h1>
       <div className="quizInfo">
-        <p>Some info about the quiz... 🤔</p>
+        <p>Let`s test your knowledge about Sweden!</p>
+        <p>
+          You get 2 points per question if your answer is correct. If you answer
+          incorrectly or don`t answer a question before the timer runs out, you
+          loose a point.
+        </p>
+        <p>Good luck!</p>
+        <p></p>
         <p>Number of questions: 10</p>
         <p>Time per question: 10 seconds</p>
-        <p>Maximum points: 20 points (2 / question)</p>
+        <p>Maximum points: 20 points</p>
         <button onClick={handleStartQuiz}>Start quiz and timer!</button>
       </div>
-      
     </div>
   );
 };

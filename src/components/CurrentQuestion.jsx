e93fbf6a0b7e38a62cf380b8280e700f8358206b
Kris Albert Lee <krisalbert@gmail.com>
2023-11-16 16:00:36 +0100

    kai-images

--- src/components/CurrentQuestion.jsx
+++ src/components/CurrentQuestion.jsx
@@ -1,18 +1,18 @@
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
 
 export const CurrentQuestion = () => {
   const question = useSelector(
-    (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
+    state => state.quiz.questions[state.quiz.currentQuestionIndex]
   );
 
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>;
   }
-  //console.log(question);
 
   return (
     <div className="questionText">
       <h1>{question.questionText}</h1>
+      <img src={`./` + question.id + `.jpg`} />;
     </div>
   );
 };
